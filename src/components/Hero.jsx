f7fbc27eea6934d057665f8190dffd425736895d
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-25 19:54:03 -0500

    add button component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react'
+import Button from './Button';
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -67,11 +68,13 @@ const Hero = () => {
 
                 <div className='absolute left-0 top-0 z-40 size-full'>
                     <div className='mt-24 px-5 sm:px-10'>
-                        <h1 className='special-font hero-heading text-blue-100'>redefi <b>n</b>e</h1>
+                        <h1 className='special-font hero-heading text-blue-100'>redefi<b>n</b>e</h1>
 
                         <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>
                             Enter the Metagame Layer <br /> Unleash the Play Economy
                         </p>
+
+                        <Button />
                     </div>
                 </div>
             </div>
