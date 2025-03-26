8606e34e1155b2439049b9732b38ebbeca0a4fbc
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-25 19:41:13 -0500

    add video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -50,6 +50,14 @@ const Hero = () => {
                         className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
                         onLoadedData={handleVideoLoad}
                     />
+
+                    <video
+                        src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+                        autoPlay
+                        loop
+                        muted
+                        className='absolute left-0 top-0 size-full object-cover object-center'
+                    />
                 </div>
             </div>
         </div>
