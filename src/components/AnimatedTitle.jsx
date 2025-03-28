98d1f845806c34a09b376421b1817b5dbc1dbb9e
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-28 17:58:32 -0500

    add useref

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -4,7 +4,10 @@ const AnimatedTitle = ({ title, containerClass }) => {
     const containerRef = useRef(null);
 
     return (
-        <div className={`animated-title ${containerClass}`}>
+        <div
+            ref={containerRef}
+            className={`animated-title ${containerClass}`}
+        >
             {title.split('<br/>').map((line, index) => (
                 <div key={index} className='flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3'>
                     {line.split(' ').map((word, i) => (
