80860d81b6543a0aa04b077e8a49d20b06e8d8ca
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-28 17:20:53 -0500

    add img

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,11 @@ const About = () => {
 
             <div className='h-dvh w-screen' id='clip'>
                 <div className='mask-clip-path about-image'>
-
+                    <img
+                        src='img/about.webp'
+                        alt="background"
+                        className='absolute left-0 top-0 size-full object-cover'
+                    />
                 </div>
             </div>
         </div>
