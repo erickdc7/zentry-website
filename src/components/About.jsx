af78d27c0ddc40107de72a55833e959194131fdf
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-27 21:01:31 -0500

    add styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const About = () => {
     return (
         <div id='about' className='min-h-screen w-screen'>
+            <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
 
+            </div>
         </div>
     )
 }
