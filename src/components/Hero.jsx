c61ea7cc0f3eaaaa8b03fbc6520b1dce0424e026
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-27 20:51:53 -0500

    add animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -70,6 +70,16 @@ const Hero = () => {
 
     return (
         <div className='relative h-dvh w-screen overflow-x-hidden'>
+
+            {isLoading && (
+                <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50'>
+                    <div className='three-body'>
+                        <div className='three-body__dot' />
+                        <div className='three-body__dot' />
+                        <div className='three-body__dot' />
+                    </div>
+                </div>
+            )}
             <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
                 <div>
                     <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
