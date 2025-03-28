26a2467136a6f17049cd6de99df0e0c77e562df4
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-27 21:03:20 -0500

    add text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,9 @@ const About = () => {
     return (
         <div id='about' className='min-h-screen w-screen'>
             <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
-                <h2 className='font-general text-sm uppercase md:text-[10px]'></h2>
+                <h2 className='font-general text-sm uppercase md:text-[10px]'>
+                    Welcome to Zentry
+                </h2>
             </div>
         </div>
     )
