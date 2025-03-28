bfbe10b1666706b01e867e9edde0b1322bacece0
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-28 17:44:11 -0500

    add gsap

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const AnimatedTitle = () => {
+const AnimatedTitle = ({ title, containerClass }) => {
     return (
-        <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-            Disc<b>o</b>ver the world's<br /> l<b>a</b>rgest shared adventure
+        <div className={`animated-title ${containerClass}`}>
+            {title.split('<br/>').map((line, index) => (
+                <div key={index} className='flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3'>
+                    {line.split(' ').map((word, i) => (
+                        <span key={i} className='animated-word' dangerouslySetInnerHTML={{ __html: word }} />
+                    ))}
+                </div>
+            ))}
         </div>
     )
 }
