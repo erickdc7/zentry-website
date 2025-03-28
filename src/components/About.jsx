0ce533d5638e0bf2bebdef8438827f02b1961c81
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-28 17:33:41 -0500

    add text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -33,11 +33,7 @@ const About = () => {
                 <h2 className='font-general text-sm uppercase md:text-[10px]'>Welcome to Zentry</h2>
 
                 <AnimatedTitle />
-
-                <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-                    Disc<b>o</b>ver the world's<br /> l<b>a</b>rgest shared adventure
-                </div>
-
+ 
                 <div className='about-subtext'>
                     <p>The Game of Games begins-your life, now an epic MMORPG</p>
                     <p>Zentry unites every player from countless games and platforms</p>
--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const AnimatedTitle = () => {
     return (
-        <div>AnimatedTitle</div>
+        <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
+            Disc<b>o</b>ver the world's<br /> l<b>a</b>rgest shared adventure
+        </div>
     )
 }
 
