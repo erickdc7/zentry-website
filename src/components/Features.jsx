00f223f888419c53cc1befb3f1f13b6a214ce28b
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-29 18:05:14 -0500

    add styles

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Features = () => {
     return (
         <section className='bg-black pb-52'>
+            <div className='container mx-auto px-3 md:px-10'>
 
+            </div>
         </section>
     )
 }
