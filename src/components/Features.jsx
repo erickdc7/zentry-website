0e8093d53226bb69b902918148851b6c3c2da1bb
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-29 18:45:21 -0500

    add styles

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -10,6 +10,9 @@ const BentoCard = ({ src, title, description, isComingSoon }) => {
                 autoPlay
                 className='absolute left-0 top-0 size-full object-cover object-center'
             />
+            <div className='relative z-10 flex size-full flex-col justify-between p-5 text-blue-50'>
+
+            </div>
             {title}
         </div>
     )
