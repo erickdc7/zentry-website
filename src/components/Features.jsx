772d0ad9a82f9a47090063aadb070943f779b2ed
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-29 18:55:12 -0500

    fix code

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -28,19 +28,19 @@ const Features = () => {
             <div className='container mx-auto px-3 md:px-10'>
                 <div className='px-5 py-32'>
                     <p className='font-circular-web text-lg text-blue-50'>Into the Metagame Layer</p>
+
+                    <p className='max-w-md font-circular-web text-lg text-blue-50 opacity-50'>
+                        Immerse yourself in a rich and ever-expanding universe where a vibrant array of products converge into an interconected overlay experience on your world.
+                    </p>
                 </div>
 
-                <p className='max-w-md font-circular-web text-lg text-blue-50 opacity-50'>
-                    Immerse yourself in a rich and ever-expanding universe where a vibrant array of products converge into an interconected overlay experience on your world.
-                </p>
-            </div>
-
-            <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
-                <BentoCard
-                    src="videos/feature-1.mp4"
-                    title={<>radi<b>n</b>t</>}
-                    description="The game of games app transforming moments across Web2 & Web3 titles into rewards."
-                />
+                <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
+                    <BentoCard
+                        src="videos/feature-1.mp4"
+                        title={<>radi<b>n</b>t</>}
+                        description="The game of games app transforming moments across Web2 & Web3 titles into rewards."
+                    />
+                </div>
             </div>
         </section>
     )
