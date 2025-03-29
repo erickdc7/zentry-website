e59d90ea3da0135fa5ea23e8891082fce93a0d42
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-29 18:06:33 -0500

    add styles

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -5,7 +5,7 @@ const Features = () => {
         <section className='bg-black pb-52'>
             <div className='container mx-auto px-3 md:px-10'>
                 <div className='px-5 py-32'>
-                    <p>Into the Metagame Layer</p>
+                    <p className='font-circular-web text-lg text-blue-50'>Into the Metagame Layer</p>
                 </div>
             </div>
         </section>
