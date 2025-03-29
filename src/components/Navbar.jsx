166e06af6cceccbe06649997b932677660e7c03a
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-28 19:32:59 -0500

    add styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,9 @@ const Navbar = () => {
         <div ref={navContainerRef} className=' fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6'>
             <header className='absolute top-1/2 w-full -translate-y-1/2'>
                 <nav className='flex size-full items-center justify-between p-4'>
+                    <div className='flex items-center gap-7'>
 
+                    </div>
                 </nav>
             </header>
         </div>
