f0c1b9ff3ed057a0268fdf08c4e6472c66b2f649
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-29 19:58:56 -0500

    add styles

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,32 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { TiLocationArrow } from 'react-icons/ti'
+
+const BentoTilt = ({ children, className = '' }) => {
+    const [transformStyle, setTransformStyle] = useState('')
+    const itemRef = useRef();
+
+    const handleMouseMove = (e) => {
+        if (!itemRef.current) return;
+
+        setTransformStyle('')
+    }
+
+    const handleMouseLeave = () => {
+        setTransformStyle('')
+    }
+
+    return (
+        <div
+            className={className}
+            ref={itemRef}
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+            style={{ transform: transformStyle }}
+        >
+            {children}
+        </div>
+    )
+}
 
 const BentoCard = ({ src, title, description }) => {
     return (
@@ -34,16 +62,58 @@ const Features = () => {
                     </p>
                 </div>
 
-                <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
+                <BentoTilt className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
                     <BentoCard
                         src="videos/feature-1.mp4"
                         title={<>radi<b>n</b>t</>}
                         description="The game of games app transforming moments across Web2 & Web3 titles into rewards."
                     />
-                </div>
+                </BentoTilt>
 
                 <div className='grid h-[135vh] grid-cols-2 grid-rows-3 gap-7'>
+                    <BentoTilt className='bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2'>
+                        <BentoCard
+                            src="videos/feature-2.mp4"
+                            title={<>zig<b>m</b>a</>}
+                            description="The NFT collection merging Zentry’s IP, AI, and gaming—pushing the boundaries of NFT innovation."
+                        />
+                    </BentoTilt>
 
+                    <BentoTilt className='bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0'>
+                        <BentoCard
+                            src="videos/feature-3.mp4"
+                            title={<>n<b>e</b>xus</>}
+                            description="The metagame portal uniting humans & AI to play, compete and earn, shaping profiles that reflect their legacy."
+                        />
+                    </BentoTilt>
+
+                    <BentoTilt className='bento-tilt_1 me-14 md:col-span-1 md:me-0'>
+                        <BentoCard
+                            src="videos/feature-4.mp4"
+                            title={<>az<b>u</b>l</>}
+                            description="The agent of agents elevating agentic AI experience to be more fun and productive."
+                        />
+                    </BentoTilt>
+
+                    <BentoTilt className='bento-tilt_2'>
+                        <div className='flex size-full flex-col justify-between bg-violet-300 p-5'>
+                            <h1 className='bento-title special-font max-w-64 text-black'>
+                                M<b>o</b>re co<b>m</b>ing s<b>o</b>on!
+                            </h1>
+
+                            <TiLocationArrow className='m-5 scale-[5] self-end' />
+                        </div>
+                    </BentoTilt>
+
+                    <BentoTilt className='bento-tilt_2'>
+                        <video
+                            src="videos/feature-5.mp4"
+                            loop
+                            muted
+                            autoPlay
+                            className='size-full object-cover object-center'
+                        />
+                    </BentoTilt>
                 </div>
             </div>
         </section>
