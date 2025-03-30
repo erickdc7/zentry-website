66e734ed2c9e96dbb93e251a5e38a0a571413b44
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-30 11:22:30 -0500

    add styles

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Story = () => {
     return (
         <section id='story' className='min-h-dvh w-screen bg-black text-blue-50'>
+            <div className='flex size-full flex-col items-center py-10 pb-24'>
 
+            </div>
         </section>
     )
 }
