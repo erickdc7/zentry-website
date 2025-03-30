4ea41b70453007512b2f41976a48405352568a1a
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-30 11:22:54 -0500

    add text

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -4,7 +4,7 @@ const Story = () => {
     return (
         <section id='story' className='min-h-dvh w-screen bg-black text-blue-50'>
             <div className='flex size-full flex-col items-center py-10 pb-24'>
-
+                <p>the multiversal ip world</p>
             </div>
         </section>
     )
