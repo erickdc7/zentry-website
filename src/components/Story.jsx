aa69aa99e55197cce20b5dfacefebafc48182a89
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-30 11:32:42 -0500

    add functions

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -4,6 +4,14 @@ import AnimatedTitle from './AnimatedTitle'
 const Story = () => {
     const frameRef = useRef('null')
 
+    const handleMouseLeave = () => {
+
+    }
+
+    const handleMouseMove = () => {
+
+    }
+
     return (
         <section id='story' className='min-h-dvh w-screen bg-black text-blue-50'>
             <div className='flex size-full flex-col items-center py-10 pb-24'>
@@ -21,6 +29,10 @@ const Story = () => {
                             <div className='story-img-content'>
                                 <img
                                     ref={frameRef}
+                                    onMouseLeave={handleMouseLeave}
+                                    onMouseUp={handleMouseLeave}
+                                    onMouseEnter={handleMouseLeave}
+                                    onMouseMove={handleMouseMove}
                                     src="/img/entrance.webp"
                                     alt="entrance"
                                     className='object-contain'
