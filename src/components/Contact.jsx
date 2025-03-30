f72d82002982dd564ed4b1e408aac806948bde67
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-30 11:56:24 -0500

    add styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Contact = () => {
     return (
         <div id='contact' className='my-20 min-h-96 w-screen px-10'>
+            <div className='relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden'>
 
+            </div>
         </div>
     )
 }
