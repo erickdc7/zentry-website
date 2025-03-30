42832b8ad1d2ba6f9284bfd8662aa7635f30ef22
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-03-30 11:18:40 -0500

    add function

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,13 +3,22 @@ import { TiLocationArrow } from 'react-icons/ti'
 
 const BentoTilt = ({ children, className = '' }) => {
     const [transformStyle, setTransformStyle] = useState('')
-    const itemRef = useRef();
+    const itemRef = useRef(null);
 
-    const handleMouseMove = (e) => {
+    const handleMouseMove = (event) => {
         if (!itemRef.current) return;
 
-        setTransformStyle('')
-    }
+        const { left, top, width, height } = itemRef.current.getBoundingClientRect();
+
+        const relativeX = (event.clientX - left) / width;
+        const relativeY = (event.clientY - top) / height;
+
+        const tiltX = (relativeY - 0.5) * 5;
+        const tiltY = (relativeX - 0.5) * -5;
+
+        const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(.95, .95, .95)`;
+        setTransformStyle(newTransform);
+    };
 
     const handleMouseLeave = () => {
         setTransformStyle('')
